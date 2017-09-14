63129133da472b0a6be31e5b73ecd236bd64d639
david90 <david.ng@oursky.com>
2017-09-14 20:29:23 +0800

    Connect to Skygear Endpoint

--- photo-app/AppDelegate.swift
+++ photo-app/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SKYKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        SKYContainer.default().configAddress("https://photoapp.skygeario.com/")
+        SKYContainer.default().configure(withAPIKey: "ae10e699980e43659ac2312c1f1c46ed")
         return true
     }
 
