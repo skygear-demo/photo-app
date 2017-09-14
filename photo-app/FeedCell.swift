73b1feaa4d33f965f294f0da44b3313a8c75ddf5
david90 <david.ng@oursky.com>
2017-09-15 03:45:46 +0800

    Setup table view and feed cell view

---
+++ photo-app/FeedCell.swift
@@ -0,0 +1,27 @@
+//
+//  FeedCell.swift
+//  photo-app
+//
+//  Created by David Ng on 15/9/2017.
+//  Copyright © 2017 Skygear. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var contentLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- photo-app/SecondViewController.swift
+++ photo-app/SecondViewController.swift
@@ -9,12 +9,15 @@
 import UIKit
 import SKYKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet var tableView: UITableView!
 
+    var posts = [AnyObject]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        loadPosts()
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,8 +35,48 @@ class SecondViewController: UIViewController {
         } else {
                 self.tableView.isHidden = true
         }
+    }
+    
+    func onCompletion(_ posts:[Any]) {
+        print("There are \(posts.count) posts")
+        print(posts)
+    }
+    
+    func loadPosts() {
+        let publicDB = SKYContainer.default().publicCloudDatabase
+        let query = SKYQuery(recordType: "post", predicate: nil)
+        let sortDescriptor = NSSortDescriptor(key: "_created_at", ascending: false)
+        query.sortDescriptors = [sortDescriptor]
         
+        publicDB.perform(query, completionHandler: { posts, error in
+            if let error = error {
+                print("Error retrieving photos: \(error)")
+                self.onCompletion(posts!)
+            } else {
+                self.onCompletion(posts!)
+            }
+        })
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //return self.posts.count
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let newsCell = tableView.dequeueReusableCell(withIdentifier: "feedcell", for: indexPath) as! FeedCell
+        
+        // Configure the cell...
+        newsCell.titleLabel.text = "Title1"
+        newsCell.contentLabel.text = "Content2"
+        
+        return newsCell
+
     }
 
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
 }
 
