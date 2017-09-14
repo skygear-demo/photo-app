0e25e4db876545bc535334966170d42b2bfbabbe
david90 <david.ng@oursky.com>
2017-09-15 00:19:49 +0800

    Implement Sign up with user name password

--- photo-app/FirstViewController.swift
+++ photo-app/FirstViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import SKYKit
 
 class FirstViewController: UIViewController {
-
+    @IBOutlet var usernameField: UITextField!
+    @IBOutlet var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,23 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    @IBAction func signupDidPress(_ sender: AnyObject) {
+        signup()
+    }
+    
+    func signup() {
+        
+        var name: String = self.usernameField.text!
+        var password: String = self.passwordField.text!
+        
+        SKYContainer.default().auth.signup(withUsername: name, password: password) { (user, error) in
+            if (error != nil) {
+                NSLog(error.debugDescription)
+                return
+            }
+            print("Signed up as: \(user)")
+        }
+    }
 }
 
